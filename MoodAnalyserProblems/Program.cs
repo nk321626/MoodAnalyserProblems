351ae5a3fdd5423a9b7c9ed3c3f9bea25fddf272
nk321626 <nk321626@gmail.com>
2022-05-21 20:59:19 +0530

    Added Welcome Message

---
+++ MoodAnalyserProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace MoodAnalyserProblems
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Problem Program");
+            Console.ReadLine();
+        }
+    }
+}
