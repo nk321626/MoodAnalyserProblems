a5b7d76dde4c8548dcffc05785e0148b68f846b5
nk321626 <nk321626@gmail.com>
2022-05-22 23:14:03 +0530

    Added Handle Exception Method Included Test cases

--- MoodAnalyserProblems/TestingMood.cs
+++ MoodAnalyserProblems/TestingMood.cs
@@ -6,11 +6,10 @@ namespace MoodAnalyserTestProject
     /// Different Test Cases For Analysing Mood
     /// </summary>
     [TestClass]
-    public class MoodAnalyserTestClass
+    public class TestingMood
     {
-        /// <summary>
-        /// TC 1.1: Given?I am in Sad Mood? message Should Return SAD.
-        /// </summary>
+        //Method to test sad message(UC1-TC1.1)
+        [TestCategory("SAD Message")]
         [TestMethod]
         public void GivenSadMessageReturnSadMood()
         {
@@ -24,10 +23,8 @@ namespace MoodAnalyserTestProject
             //Assert
             Assert.AreEqual(expected, actual);
         }
-
-        /// <summary>
-        /// TC 1.2: Given?I am in Happy Mood? message Should Return HAPPY.
-        /// </summary>
+        //Method to test happy message(UC1-TC1.2)
+        [TestCategory("HAPPY Message")]
         [TestMethod]
         public void GivenHappyMessageReturnHappyMood()
         {
@@ -39,6 +36,21 @@ namespace MoodAnalyserTestProject
             //Act
             string actual = mood.AnalyserMood();
             //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        //Method to test happy message(UC2-TC2.1)
+        [TestCategory("Null Exception")]
+        [TestMethod]
+        public void GivenNullMessageException()
+        {
+            ///AAA
+            ///Arange
+            string msg = null;
+            string expected = "happy";
+            MoodAnalyser mood = new MoodAnalyser(msg);
+            //Act
+            string actual = mood.AnalyserMood();
+            ///Asert
             Assert.AreEqual(expected, actual);
         }
     }
